c4f09cffbae2625f6c19cc8c5819cb76ed6628da
kaki li <kaki.noodles@gmail.com>
2019-12-09 15:24:18 +0800

    done user page and some design fix

---
+++ utils/login.js
@@ -0,0 +1,13 @@
+// wx.getUserInfo({
+//   withCredentials: true,
+//   lang: '',
+//   success: function(res) {},
+//   fail: function(res) {},
+//   complete: function(res) {},
+// })
+
+// onGotUserInfo: function (e) {
+//   console.log(e.detail.errMsg)
+//   console.log(e.detail.userInfo)
+//   console.log(e.detail.rawData)
+// },
